5541a7414c87d7bafaf0509429251ec812879996
nyh3 <e0970473@u.nus.edu>
2023-05-26 22:33:54 +0800

    added styling

---
+++ orbital-app/app/constants/drawers.jsx
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'restrictions',
+        iconType: 'Material',
+        iconName:'face-profile'
+    },
+    {
+        name:'promotions',
+        iconType:'Feather',
+        iconName:'settings'
+    },
+    {
+        name:'saved',
+        iconType:'Material',
+        iconName:'bookmark-check-outline'
+    },
+    {
+        name:'reviews',
+        iconType:'FontAwesome5',
+        iconName:'user-friends'
+    }
+]
