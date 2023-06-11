568f67353187e7df5dc35185dfd425b9ac1a4fc4
nyh3 <e0970473@u.nus.edu>
2023-06-11 16:58:12 +0800

    removed useContexts that were no longer needed

--- orbital-app/app/_layout.jsx
+++ orbital-app/app/_layout.jsx
@@ -1,18 +1,12 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/auth";
 import { GroupProvider } from "../contexts/group";
-import { StallProvider } from "../contexts/stallid";
-import { MenuProvider } from "../contexts/menuid";
 
 export default function Root() {
     return (
         <GroupProvider>
             <AuthProvider>
-                <StallProvider>
-                    <MenuProvider>
-                        <Slot />
-                    </MenuProvider>
-                </StallProvider>
+                <Slot />
             </AuthProvider>
         </GroupProvider>
     );
