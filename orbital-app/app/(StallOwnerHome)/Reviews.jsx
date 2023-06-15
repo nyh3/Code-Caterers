d4d5ec88d2904b0f880ba11ee0b109a281f5694f
nyh3 <e0970473@u.nus.edu>
2023-06-16 01:03:59 +0800

    reviews for owner show the menu which can be pressed to see the reviews for that menu

--- orbital-app/app/(StallOwnerHome)/Reviews.jsx
+++ orbital-app/app/(StallOwnerHome)/Reviews.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Image, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Image, View, StyleSheet, Text, TouchableOpacity, FlatList } from "react-native";
 import { ListItem } from 'react-native-elements';
 import { supabase } from '../../lib/supabase';
 import { useAuth } from "../../contexts/auth";
@@ -8,6 +8,7 @@ export default function ReviewPage() {
     const userId = useAuth();
     const [reviews, setReviews] = useState([]);
     const [menuReviewsVisibility, setMenuReviewsVisibility] = useState({});
+    const [menu, setMenu] = useState([]);
 
     useEffect(() => {
         fetchStallId();
@@ -28,20 +29,24 @@ export default function ReviewPage() {
 
     const fetchMenuId = async (stallId) => {
         const { data, error } = await supabase
-            .from('Menu')
-            .select('id')
-            .eq('stall_id', stallId);
-
+          .from('Menu')
+          .select('id, name')
+          .eq('stall_id', stallId);
+      
         if (!error) {
-            const menuId = data.map((menu) => menu.id);
-            fetchReviewId(menuId);
+          const menuData = data.reduce((acc, menu) => {
+            acc[menu.id] = { id: menu.id, name: menu.name };
+            return acc;
+          }, {});
+          fetchReviewId(Object.keys(menuData));
+          setMenu(menuData);
         }
-    };
-
+      };
+      
     const fetchReviewId = async (menuId) => {
         const { data, error } = await supabase
             .from('review')
-            .select('id, menu_id')
+            .select('id, menu_id, rating, review_text')
             .in('menu_id', menuId);
 
         if (!error) {
@@ -54,26 +59,30 @@ export default function ReviewPage() {
           ...prevVisibility,
           [menuId]: !prevVisibility[menuId],
         }));
-      };
-    
-
-    const renderMenuReviews = (menuId) => {
-        const menuReviews = reviews.filter((review) => review.menu_id === menuId);
-
-        if (menuReviewsVisibility[menuId]) {
-            return (
-                <View>
-                {menuReviews.map((review) => (
-                  <TouchableOpacity key={review.id} onPress={() => toggleMenuReviewsVisibility(review.id)}>
-                    <Text>{review.rating}</Text>
-                    {review.id === menuId && <Text>{review.comment}</Text>}
-                  </TouchableOpacity>
-                ))}
-              </View>  
-            );
+      };      
+      
+      const renderMenuReviews = (menuId) => {
+        const isVisible = menuReviewsVisibility[menuId];
+      
+        if (isVisible) {
+          const menuReviews = reviews.filter((review) => review.menu_id === menuId);
+      
+          return (
+            <View>
+              {menuReviews.map((review) => (
+                <TouchableOpacity key={`${menuId}-${review.id}`}>
+                  <Image source={{ uri: review.image }} style={styles.reviewImage} />
+                  <Text>{review.rating}</Text>
+                  <Text>{review.review_text}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          );
         }
         return null;
-    };
+      };
+      
+    
     return (
         <View style={styles.container}>
             <Text style={styles.header}>
@@ -82,14 +91,18 @@ export default function ReviewPage() {
             <Text style={styles.ratings}>
                 Ratings: use database to calculate overall ratings
             </Text>
-            {reviews.map((review) => (
-            <ListItem key={review.id} onPress={() => toggleMenuReviewsVisibility(review.menu_id)}>
-            <ListItem.Content>
-                <ListItem.Title>{review.menu_id}</ListItem.Title>
-                {renderMenuReviews(review.menu_id)}
-            </ListItem.Content>
-            </ListItem>
-            ))}
+            <FlatList
+                data={Object.values(menu)}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <ListItem onPress={() => toggleMenuReviewsVisibility(item.id)}>
+                    <ListItem.Content>
+                        <ListItem.Title>{item.name}</ListItem.Title>
+                        {renderMenuReviews(item.id)}
+                    </ListItem.Content>
+                    </ListItem>
+                )}
+                />
         </View>
     )
 }
