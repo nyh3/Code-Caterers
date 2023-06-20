84aac362bc210660d06307e2718daac65ca40ed9
nyh3 <e0970473@u.nus.edu>
2023-06-20 19:22:01 +0800

    finalize home page styling for now

--- orbital-app/app/(home)/index.jsx
+++ orbital-app/app/(home)/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList, Text, Image, TouchableOpacity, TextInput } from 'react-native';
 import { supabase } from '../../lib/supabase';
 import { useIsFocused } from '@react-navigation/native';
@@ -23,7 +23,7 @@ export default function StallPage() {
     try {
       const { data, error } = await supabase
         .from('Stall')
-        .select('*, location ( name )')
+        .select('*, location ( name ), cuisine (name)')
         .ilike('name', `%${searchQuery}%`);
       if (error) {
         console.error('Error fetching stall details:', error.message);
@@ -39,20 +39,49 @@ export default function StallPage() {
     router.push({ pathname: '/stallDetails', params: { id: stall } });
   };
 
+  const getCuisineTagColor = (cuisineName) => {
+    switch (cuisineName) {
+      case 'Chinese':
+        return '#FFD700'; // Gold color for Chinese cuisine
+      case 'Western':
+        return '#6495ED'; // Cornflower blue color for Western cuisine
+      case 'Malay':
+        return '#228B22'; // Forest green color for Malay cuisine
+      case 'Indian':
+        return '#FF4500'; // Orange-red color for Indian cuisine
+      case 'Japanese':
+        return '#FF69B4'; // Hot pink color for Japanese cuisine
+      case 'Korean':
+        return '#CD5C5C'; // Indian red color for Korean cuisine
+      case 'Thai':
+        return '#FFA500'; // Orange color for Thai cuisine
+      default:
+        return '#CCCCCC'; // Default color if cuisine name doesn't match any specific case
+    }
+  };
+
   const renderStall = ({ item }) => (
     <TouchableOpacity onPress={() => handleStallPress(item.id)}>
-      <View style={styles.stall}>
+      <View style={styles.stallContainer}>
         <Image source={{ uri: item.stallImage }} style={styles.stallImage} />
         <View style={styles.stallDetails}>
           <Text style={styles.stallTitle}>{item.name} @ {item.location.name}</Text>
-          <AirbnbRating
-            defaultRating={parseFloat(item.rating) || 0}
-            size={20}
-            isDisabled
-            showRating={false}
-            minRating={0}
-            maxRating={5}
-          />
+          <View style={styles.ratingContainer}>
+            <AirbnbRating
+              defaultRating={parseFloat(item.rating) || 0}
+              size={20}
+              isDisabled
+              showRating={false}
+              minRating={0}
+              maxRating={5}
+            />
+            <Text style={styles.ratingText}>{parseFloat(item.rating) || 0} / 5.0</Text>
+          </View>
+          <View style={styles.cuisineTagsContainer}>
+            <Text style={[styles.cuisineTag, { backgroundColor: getCuisineTagColor(item.cuisine.name) }]}>
+              {item.cuisine.name}
+            </Text>
+          </View>
         </View>
       </View>
     </TouchableOpacity>
@@ -83,7 +112,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     backgroundColor: '#FFF5FA',
     flex: 1,
-    marginHorizontal: 10,
+    paddingHorizontal: 10,
   },
   heading: {
     fontSize: 20,
@@ -93,29 +122,61 @@ const styles = StyleSheet.create({
   stallList: {
     paddingBottom: 20,
   },
-  stall: {
-    flexDirection: 'row',
+  stallContainer: {
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
     marginBottom: 15,
+    elevation: 3,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
   },
   stallImage: {
-    width: 100,
-    height: 100,
-    marginRight: 10,
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
   stallDetails: {
-    flex: 1,
-    alignSelf: 'center',
-    marginHorizontal: 10,
+    padding: 10,
   },
   stallTitle: {
     fontWeight: 'bold',
-    fontSize: 15,
+    fontSize: 18,
+    marginBottom: 10,
   },
-  stallDescription: {
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 5,
+  },
+  ratingText: {
+    marginLeft: 5,
+    fontSize: 16,
+  },
+  cuisineTagsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 5,
+  },
+  cuisineTag: {
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginTop: 5,
+    marginRight: 5,
+    backgroundColor: '#CCCCCC',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 12,
   },
   searchInput: {
     marginBottom: 10,
     fontSize: 15,
-  }
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+  },
 });
