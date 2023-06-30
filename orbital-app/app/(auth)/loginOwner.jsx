3699f67da27bec885cbc71c5a649da28d6dceec7
nyh3 <e0970473@u.nus.edu>
2023-07-01 01:55:54 +0800

    edited the error message so that when there is a successful login the error message disappears before login in, not sure if this is enough

--- orbital-app/app/(auth)/loginOwner.jsx
+++ orbital-app/app/(auth)/loginOwner.jsx
@@ -20,6 +20,7 @@ export default function LoginPage() {
             return;
         }
         setLoading(true);
+        setErrMsg('');
         const { error } = await supabase.auth.signInWithPassword({ email, password });
         setLoading(false);
         if (error) {
