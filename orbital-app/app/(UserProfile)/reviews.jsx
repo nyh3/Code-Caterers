f4d1b4f8b451aad8e6858b56a409b83f8f5afc08
nyh3 <e0970473@u.nus.edu>
2023-07-17 14:27:08 +0800

    more fixes

--- orbital-app/app/(UserProfile)/reviews.jsx
+++ orbital-app/app/(UserProfile)/reviews.jsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet, Text, Image, FlatList, TouchableOpacity } from "react-native";
-import { Button, ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator } from "react-native-paper";
 import { supabase } from "../../lib/supabase";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/auth";
@@ -41,9 +41,6 @@ export default function ReviewsPage() {
     router.push({ pathname: '/editReview', params: { id: reviewId } });
   };
 
-  const reviewedReviews = reviews.filter(review => review.rating !== null);
-  const nullReviews = reviews.filter(review => review.rating === null);
-
   const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => handleEditReview(item.id)} style={styles.reviewItem}>
       <View>
@@ -72,27 +69,13 @@ export default function ReviewsPage() {
         <ActivityIndicator />
       ) : (
         <>
-          {nullReviews.length > 0 ? (
-            <View>
-              <Text style={styles.sectionTitle}>Null Reviews</Text>
-              {nullReviews.map(review => (
-                <View style={styles.nullReviewItem} key={review.id}>
-                  <Text style={styles.nullReviewText}>You have not reviewed this item yet</Text>
-                  <Button onPress={() => handleEditReview(review.id)}>Edit Review</Button>
-                </View>
-              ))}
-            </View>
-          ) : null}
-          {reviewedReviews.length > 0 ? (
-            <View>
-              <Text style={styles.sectionTitle}>Reviews written: </Text>
-              <FlatList
-                data={reviewedReviews}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-                contentContainerStyle={styles.flatListContent}
-              />
-            </View>
+          {reviews.length > 0 ? (
+            <FlatList
+              data={reviews}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id.toString()}
+              contentContainerStyle={styles.flatListContent}
+            />
           ) : (
             <Text style={styles.noReviews}>User has not written any reviews.</Text>
           )}
@@ -110,11 +93,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 15,
   },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
   reviewItem: {
     backgroundColor: '#FFECF6',
     padding: 15,
@@ -128,14 +106,6 @@ const styles = StyleSheet.create({
   },
   comment: {
     fontSize: 16,
-  },
-  nullReviewItem: {
-    marginBottom: 20,
-  },
-  nullReviewText: {
-    fontSize: 16,
-    fontStyle: 'italic',
-    marginBottom: 10,
   },
   reviewImage: {
     width: 70,
