e2b1206efe05fd78a6b4789a0edbd6244fdf0102
nyh3 <e0970473@u.nus.edu>
2023-07-23 22:44:24 +0800

    added sytem testing

--- orbital-app/app/test/stallprofile.js
+++ orbital-app/app/test/stallprofile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import { Text, TextInput, Button, Menu, Provider, ActivityIndicator } from 'react-native-paper';
 
